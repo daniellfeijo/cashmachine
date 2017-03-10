a04feffdadad8d962e8dc57cdb3a74e747a83b82
Daniel <daniellfeijo@gmail.com>
2017-03-10 12:06:15 -0500

    first files

---
+++ CashMachine/CashMachine/Model/Checking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine.Model
+{
+    class Checking
+    {
+        public int Numero { get; private set; }
+        public double Saldo { get; private set; }
+        public Client client { get; private set; }
+
+        public void withdraw(double saldo)
+        {
+            this.Saldo -= saldo;
+        }
+
+    }
+}
