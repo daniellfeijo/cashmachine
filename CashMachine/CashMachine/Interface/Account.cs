3159193a138852cb01e60a51e39e35381245a45d
Daniel <daniellfeijo@gmail.com>
2017-03-10 12:52:08 -0500

    StyleCop

---
+++ CashMachine/CashMachine/Interface/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashMachine.Interface
+{
+    public interface Account
+    {
+        double Balance { get; }
+
+        void Withdraw(double value);
+        void Deposit(double value);
+    }
+}
--- CashMachine/CashMachine/Model/Checking.cs
+++ CashMachine/CashMachine/Model/Checking.cs
@@ -1,21 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// <copyright file="Checking.cs" company="daniellfeijo.com">
+//     daniellfeijo.com. All rights reserved.
+// </copyright>
+// <author>Daniel Feijo</author>
 namespace CashMachine.Model
-{
-    class Checking
+{ 
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using CashMachine.Interface;
+    
+    /// <summary>
+    /// The model class for Checking
+    /// </summary>
+    public class Checking : Account
     {
+        /// <summary>
+        /// Gets Number of the checking account
+        /// </summary>
         public int Numero { get; private set; }
-        public double Saldo { get; private set; }
-        public Client client { get; private set; }
+        
+        /// <summary>
+        /// Gets Balance of checking account
+        /// </summary>
+        public double Balance { get; private set; }
+        
+        /// <summary>
+        /// Gets Client of checking account
+        /// </summary>
+        public Client Client { get; private set; }
 
-        public void withdraw(double saldo)
+        /// <summary>
+        /// Method to Withdraw the value
+        /// </summary>
+        /// <param name="value">Value for withdraw</param>
+        public void Withdraw(double value)
         {
-            this.Saldo -= saldo;
+            this.Balance -= value;
         }
 
+        /// <summary>
+        /// Method to Deposit the value
+        /// </summary>
+        /// <param name="value">Value to deposit</param>
+        public void Deposit(double value)
+        {
+            this.Balance += value;
+        }
+
+        /// <summary>
+        /// Method to transfer the value
+        /// </summary>
+        /// <param name="value">Value for transfer</param>
+        /// <param name="account">Account that receive the value</param>
+        public void Transfer(double value, Account account)
+        {
+            this.Withdraw(value);
+            account.Deposit(value);
+        }
     }
 }
