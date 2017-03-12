ea8e52e24000ca3916f4951d4778add53b4133fd
Daniel <daniellfeijo@gmail.com>
2017-03-12 15:23:28 -0400

    add account in database working

---
+++ CashMachine/CashMachine/Migrations/20170312191547_CreateClientAndAccount.Designer.cs
@@ -0,0 +1,97 @@
+using System;
+using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using Microsoft.Data.Entity.Metadata;
+using Microsoft.Data.Entity.Migrations;
+using CashMachine;
+
+namespace CashMachine.Migrations
+{
+    [DbContext(typeof(EntitiesContext))]
+    [Migration("20170312191547_CreateClientAndAccount")]
+    partial class CreateClientAndAccount
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("CashMachine.Client", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Adress");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("ID");
+                });
+
+            modelBuilder.Entity("CashMachine.Model.Account", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<double>("Balance");
+
+                    b.Property<int>("ClientID");
+
+                    b.Property<string>("Discriminator")
+                        .IsRequired();
+
+                    b.Property<int>("Number");
+
+                    b.HasKey("ID");
+
+                    b.HasAnnotation("Relational:DiscriminatorProperty", "Discriminator");
+
+                    b.HasAnnotation("Relational:DiscriminatorValue", "Account");
+                });
+
+            modelBuilder.Entity("CashMachine.Model.CheckingAccount", b =>
+                {
+                    b.HasBaseType("CashMachine.Model.Account");
+
+
+                    b.HasAnnotation("Relational:DiscriminatorValue", "CheckingAccount");
+                });
+
+            modelBuilder.Entity("CashMachine.Model.InvestimentAccount", b =>
+                {
+                    b.HasBaseType("CashMachine.Model.Account");
+
+
+                    b.HasAnnotation("Relational:DiscriminatorValue", "InvestimentAccount");
+                });
+
+            modelBuilder.Entity("CashMachine.Model.SavingsAccount", b =>
+                {
+                    b.HasBaseType("CashMachine.Model.Account");
+
+
+                    b.HasAnnotation("Relational:DiscriminatorValue", "SavingsAccount");
+                });
+
+            modelBuilder.Entity("CashMachine.Model.Account", b =>
+                {
+                    b.HasOne("CashMachine.Client")
+                        .WithMany()
+                        .HasForeignKey("ClientID");
+                });
+
+            modelBuilder.Entity("CashMachine.Model.CheckingAccount", b =>
+                {
+                });
+
+            modelBuilder.Entity("CashMachine.Model.InvestimentAccount", b =>
+                {
+                });
+
+            modelBuilder.Entity("CashMachine.Model.SavingsAccount", b =>
+                {
+                });
+        }
+    }
+}
---
+++ CashMachine/CashMachine/Migrations/20170312191547_CreateClientAndAccount.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Entity.Migrations;
+using Microsoft.Data.Entity.Metadata;
+
+namespace CashMachine.Migrations
+{
+    public partial class CreateClientAndAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Client",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Adress = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Client", x => x.ID);
+                });
+            migrationBuilder.CreateTable(
+                name: "Account",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Balance = table.Column<double>(nullable: false),
+                    ClientID = table.Column<int>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    Number = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Account", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Account_Client_ClientID",
+                        column: x => x.ClientID,
+                        principalTable: "Client",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("Account");
+            migrationBuilder.DropTable("Client");
+        }
+    }
+}
